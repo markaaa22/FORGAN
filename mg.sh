a2ad7a05ed76f84bd7dcf00cd0af315fd327f9c6
Alireza Koochali <alireza.koochali@iav.de>
2020-10-23 18:41:42 +0200

    update number of steps

--- mg.sh
+++ mg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 forgan.py -ds mg -t lstm -steps 500000 -n 4 -c 32 -rg 64 -rd 256 -d_iter 6 -hbin 50 -hmin 0.4 -hmax 1.4
+python3 forgan.py -ds mg -t lstm -steps 50000 -n 4 -c 32 -rg 64 -rd 256 -d_iter 6 -hbin 50 -hmin 0.4 -hmax 1.4
